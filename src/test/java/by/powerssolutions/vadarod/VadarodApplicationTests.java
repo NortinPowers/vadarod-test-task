d531c49fa22c42b15703c26bff9e221db4a4472b
NortinPowers <a.nortin@gmail.com>
2024-06-28 16:32:23 +0300

    feat: add structure and packages

---
+++ src/test/java/by/powerssolutions/vadarod/VadarodApplicationTests.java
@@ -0,0 +1,13 @@
+package by.powerssolutions.vadarod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VadarodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
