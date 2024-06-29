cebb27726bd6c4b8c001fddc8a7100f07ced46d6
NortinPowers <a.nortin@gmail.com>
2024-06-29 09:58:39 +0300

    feat: add db config + 001-changelog

---
+++ src/main/java/by/powerssolutions/vadarod/domain/Rate.java
@@ -0,0 +1,13 @@
+package by.powerssolutions.vadarod.domain;
+
+import java.time.LocalDateTime;
+
+public class Rate {
+
+    private Long curId;
+    private LocalDateTime date;
+    private String curAbbreviation;
+    private Integer curScale;
+    private String curName;
+    private Double curOfficialRate;
+}
